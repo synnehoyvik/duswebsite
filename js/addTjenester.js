ba9fbaa02d98c3f599d05ce8f0d790e88230e391
synnespj <synne@arealize.ai>
2025-01-21 16:02:30 +0100

    lagt til innhold i tjenester

--- js/addTjenester.js
+++ js/addTjenester.js
@@ -1,30 +1,41 @@
-  function addToPortfolio(image, h1Text, beskrivelse) {
+function addToTjenester(image, h1Text, beskrivelseTekst) {
+  // Hent hoveddiv for tjenester
   let tjenester = document.getElementById("tjenester");
-  let tjenesterboks = document.createElement("div");
-  tjenesterboks.setAttribute("class", "tjenesterboks");
 
+  // Opprett tjenesterboks
+  let tjenesterboks = document.createElement("div");
+  tjenesterboks.setAttribute("class", "tjenesterboks");//div nr 2, med bilde og tekst
+
+  // Opprett containertjenester (tekst-container)
   let containertjenester = document.createElement("div");
   containertjenester.setAttribute("class", "containertjenester");
 
+  // Opprett overskrift (h1)
+  let h1 = document.createElement("h1");
+  h1.innerHTML = h1Text;
+
+  //tjenesterboks.appendChild(h1);
+
+  // Opprett beskrivelse
+  let beskrivelseDiv = document.createElement("div");
+  beskrivelseDiv.setAttribute("class", "beskrivelse");
+  beskrivelseDiv.innerHTML = beskrivelseTekst;
+
+  // Legg til h1 og beskrivelse inne i containertjenester
+  containertjenester.appendChild(h1);
+  containertjenester.appendChild(beskrivelseDiv);
+
+  // Opprett bilde-element
   let img = document.createElement("img");
   img.setAttribute("src", image);
   img.setAttribute("class", "imagetjenester");
   img.setAttribute("alt", "tjenestebilde");
 
-  // Legg til tekst-div for bildeteksten
-  let beskrivelse = document.createElement("div");
-  beskrivelse.setAttribute("class", "beskrivelse");
-  beskrivelse.innerHTML = beskrivelse;
-  containertjenester.appendChild(beskrivelse); //slutt bildetekst add
+  // Legg til containertjenester først, deretter img i tjenesterboks
+  tjenesterboks.appendChild(containertjenester); // Teksten først
+  tjenesterboks.appendChild(img);// Bildet til høyre
 
-
-
-  tjenesterboks.appendChild(containertjenester);
-
-  let h1 = document.createElement("h1");
-  h1.innerHTML = h1Text;
-  tjenesterboks.appendChild(h3);
-
+  // Legg tjenesterboks til hoveddiven
   tjenester.appendChild(tjenesterboks);
 }
 
